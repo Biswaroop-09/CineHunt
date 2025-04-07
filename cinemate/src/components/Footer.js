115a25545567ea5aebb7dc1cf6540849cf4c4cbe
Biswaroop-09 <112316264+Biswaroop-09@users.noreply.github.com>
2025-04-07 21:06:18 +0530

    Update Footer.js

--- cinemate/src/components/Footer.js
+++ cinemate/src/components/Footer.js
@@ -7,7 +7,7 @@ export const Footer = () => {
 
 <footer className="bg-white dark:bg-gray-900 shadow-sm m-4 ">
     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-gray-500 dark:text-gray-400  sm:text-center ">© 2030 <Link to="/" className="hover:underline">Cinemate</Link>. All Rights Reserved.
+      <span className="text-sm text-gray-500 dark:text-gray-400  sm:text-center ">© 2030 <Link to="/" className="hover:underline">CineHunt</Link>. All Rights Reserved ~ Biswaroop
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
